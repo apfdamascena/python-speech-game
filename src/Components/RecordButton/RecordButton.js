4d7108630e028ce6f00784a1248d0e016eb8d5da
apfdamascena <apfd@cin.ufpe.br>
2020-06-17 02:41:08 -0300

    recording

--- src/Components/RecordButton/RecordButton.js
+++ src/Components/RecordButton/RecordButton.js
@@ -1,28 +1,38 @@
 import React, { Component } from 'react';
 import './RecordButton.css'
 
-class RecordButton extends Component{
-    constructor(props){
+class RecordButton extends Component {
+    constructor(props) {
         super(props);
         this.state = {
             isRecording: false
         }
     }
+
+    startRecording = () => {
+        this.setState({ isRecording: !this.state.isRecording });
+    }
+
+    stopRecording = () => {
+        this.setState({ isRecording: !this.state.isRecording });
+    }
+
     switchButton = () => {
-        this.setState({isRecording:!this.state.isRecording});
+        this.setState({ isRecording: !this.state.isRecording });
         this.props.handleButtonPressed(this.state.isRecording);
     };
+
     render(){
         return(
             <div className="recordClass">
-                <button 
-                className={this.state.isRecording?"Rec":"notRec"}
-                id="recButton"
-                onClick={this.switchButton}>
+                <button
+                    className={this.state.isRecording ? "Rec" : "notRec"}
+                    id="recButton"
+                    onClick={this.switchButton}>
                 </button>
                 <div className="record">
-                    <h2 id = "pharseRecord">{this.state.isRecording? "RECORDING" : "RECORDER"}</h2>
-                </div>   
+                    <h2 id="pharseRecord">{this.state.isRecording ? "RECORDING" : "RECORDER"}</h2>
+                </div>
             </div>
         );
     }
