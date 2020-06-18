8c7d08b54f63ffbf69415eb5b4196a030f54b4b1
apfdamascena <apfd@cin.ufpe.br>
2020-06-17 22:47:16 -0300

    update

--- src/Components/RecordButton/RecordButton.js
+++ src/Components/RecordButton/RecordButton.js
@@ -1,34 +1,61 @@
 import React, { Component } from 'react';
 import './RecordButton.css'
+import MicRecorder from 'mic-recorder-to-mp3';
+
+const recorder = new MicRecorder({ bitRate: 128 });
 
 class RecordButton extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            isRecording: false
+            isRecording: false,
+            blobURL: '',
+            isBlocked: false,
         }
     }
 
-    startRecording = () => {
-        this.setState({ isRecording: !this.state.isRecording });
+    start = () => {
+        if (this.state.isBlocked) {
+            console.log('Permission Denied');
+        } else {
+            recorder.start().then(() => {
+                this.setState({ isRecording: true });
+            }).catch((error) => console.error(error));
+        }
     }
 
-    stopRecording = () => {
-        this.setState({ isRecording: !this.state.isRecording });
+    stop = () => {
+        recorder.stop().getMp3().then(([buffer, blob]) => {
+            const blobURL = URL.createObjectURL(blob)
+            this.setState({ blobURL, isRecording: false });
+        }).catch((error) => console.log(error));
     }
 
-    switchButton = () => {
-        this.setState({ isRecording: !this.state.isRecording });
-        this.props.handleButtonPressed(this.state.isRecording);
-    };
+    // switchButton = () => {
+    //     this.setState({ isRecording: !this.state.isRecording });
+    //     this.props.handleButtonPressed(this.state.isRecording);
+    // };
 
-    render(){
-        return(
+    componentDidMount() {
+        navigator.mediadevices.getUserMedia({ audio: true },
+          () => {
+            console.log('Permission Granted');
+            this.setState({ isBlocked: false });
+          },
+          () => {
+            console.log('Permission Denied');
+            this.setState({ isBlocked: true })
+          },
+        );
+    }
+
+    render() {
+        return (
             <div className="recordClass">
                 <button
                     className={this.state.isRecording ? "Rec" : "notRec"}
                     id="recButton"
-                    onClick={this.switchButton}>
+                    onClick={!this.state.isRecording ? this.start() : this.stop()}>
                 </button>
                 <div className="record">
                     <h2 id="pharseRecord">{this.state.isRecording ? "RECORDING" : "RECORDER"}</h2>
