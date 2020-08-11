313800d3fe3ceddf922022e3126949295a690734
apfdamascena <apfd@cin.ufpe.br>
2020-08-11 16:09:31 -0300

    gettind ideia for rankingpage

---
+++ src/Components/RankingPage/RankingPage.js
@@ -0,0 +1,18 @@
+import React, {Component} from  'react';
+
+class RankingPage extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+
+        }
+    }
+
+    render(){
+        return(
+            <h2>hello world</h2>
+        );
+    }
+}
+
+export default RankingPage;
