54b78c652930fff2cc6da8fec73ed1aabdc08b76
apfdamascena <apfd@cin.ufpe.br>
2020-11-28 00:19:26 -0300

    docker compose ok

--- src/Components/Pages/PrivacyPolicyPage/PrivacyPolicyPage.js
+++ src/Components/Pages/PrivacyPolicyPage/PrivacyPolicyPage.js
@@ -41,7 +41,7 @@ export default class PrivacyPolicyPage extends Component{
                         <p id="box-inside">-When creating an account on the site, you provide us with personal information that includes your email, name and a password. Later, this information allows us to create resources, such as: ranking, scores and levels reached on the platform.</p>
 
                         <p>Information we collect when you use our services:</p>
-                        <p id="box-inside" > -voice and audio information when you use audio features.<br/>
+                        <p id="box-inside" > -Voice and audio information when you use audio features.<br/>
                             - Information on activities carried out on the website.
                         
                         </p>
@@ -50,7 +50,7 @@ export default class PrivacyPolicyPage extends Component{
                         <p>Why do we collect this data?</p>
 
                         <p id="box-inside">
-                        - The audios will be used, exclusively, for a research of scientific initiation, which has the objective of programming by voice. We do not have the identification of the people who sent the audio, let alone use them for other purposes. Thus, we just want to build a large database that allows for the continuation of this research.<br/>
+                        - The audios will be used, exclusively, for a research of scientific initiation, which has the objective of programming by voice. We do not have the identification of the people who sent the audio, we don't use these audios for other purposes. We just want to build a large database that allows for the continuation of this research.<br/>
                         - We collect other information, such as: how many times a person contributed to our database, in order to build a ranking and have ways of encouraging user engagement on the platform.
                         </p>
                         </div>
