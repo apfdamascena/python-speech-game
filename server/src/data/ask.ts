ff605d147bbce39893731482446f0932ea290ccf
apfdamascena <apfd@cin.ufpe.br>
2020-12-04 03:33:57 -0300

    removing server from root

--- server/src/data/ask.ts
+++
@@ -1,8 +0,0 @@
-export default{
-    "ASK": [
-        "READ WHILE YOU RECORD",
-        "TRY TO RECORD THIS EXPRESSION",
-        "LET'S TRY TO RECORD",
-        "HOW WOULD YOU IMPLEMENT?"
-    ]
-}
